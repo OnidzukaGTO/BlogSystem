7abe654fae997ca7d8452761ad5272f5abe8670f
OnidzukaGTO <140325818+OnidzukaGTO@users.noreply.github.com>
2023-07-29 21:10:27 +0200

    model 1

---
+++ app/Models/Blog.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'title', 'content', 'published','published_at',
+    ];
+
+    protected $cats=[
+        'published'=> 'boolean',
+    ];
+    protected $dates = [
+        'published_at',
+    ];
+}
---
+++ app/Models/Currency.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Currency extends Model
+{
+    use HasFactory;
+
+    //protected $table = 'name table';
+    //protected $primaryKey = 'uuid';
+    //protected $connection = 'name db';
+
+    public $incrementing = false;
+
+    protected $fillable = [
+        'name','price','active','sort',
+    ];
+}
